814bfdbdd3ceb7af2852106836c23ad3e7326fdf
UtkarshWizard <utkarshr1214@gmail.com>
2026-01-12 21:41:20 +0530

    minor fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono , Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./provider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const poppins = Poppins({
   weight: ["100" , "200" , "300" , "400" , "500" , "600" , "700" , "800" , "900"],
