56ad236f94eebc2331213554340a508c36e7a8fe
UtkarshWizard <utkarshr1214@gmail.com>
2026-01-19 23:09:33 +0530

    dashboard ui

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,18 +1,15 @@
 "use client";
 
-import { Suspense, useEffect } from "react";
+import { useEffect } from "react";
 import { motion } from "framer-motion";
 import { Skeleton } from "@/components/ui/skeleton";
-import { RecentJournalEntries } from "../components/recentJournal";
-import { MoodTracker } from "../components/todaysmood";
-import { JournalQuickEntry } from "../components/journalEntry";
 import { WelcomeBanner } from "../components/welcome-banner";
 import NavBar from "../components/NavBar-Dashboard";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor";
-import { CirclePlus, Flame, NotebookPen } from "lucide-react";
+import { CirclePlus, Flame, Notebook, NotebookPen } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 
 const containerVariants = {
   hidden: { opacity: 0 },
@@ -71,6 +68,7 @@ export default function DashboardPage() {
               <WelcomeBanner />
             </div>
           </motion.div>
+
           <div className="grid md:grid-cols-2 grid-cols-1 gap-4 w-full lg:max-w-6xl max-w-4xl mx-auto justify-center">
             <div className="flex items-center justify-between border border-gray-700 dark:bg-gray-900 bg-gray-100 rounded-xl p-4">
               <div className="bg-orange-500/20 rounded-full p-3 mb-3">
@@ -92,13 +90,43 @@ export default function DashboardPage() {
               </div>
             </div>
           </div>
-          <div className="text-right">
+
+          <div className="flex justify-between items-center">
+            <div className="flex items-center gap-2">
+              <Notebook />
+              <span className="text-xl">Recent Journals</span>
+            </div>
             <Button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-sm border border-slate-800 dark:border-slate-600 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-gray-200 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
               <div className="flex justify-center items-center gap-4">
                 <CirclePlus className="text-orange-400 hover:text-orange-600 !h-4 !w-4" />
                 <div className="text-md">New Journal</div>
               </div>
             </Button>
+          </div>
+
+          <div className="grid grid-cols-3 justify-between items-center gap-4">
+            <div className="col-span-2 border-2 border-gray-700 h-full w-full rounded-md p-4">
+              <div className="bg-gray-800 text-white rounded-md p-4 flex flex-col gap-4">
+                <div className="flex items-center justify-between gap-4">
+                  <div className="flex flex-col gap-2">
+                    <span className="text-lg">Title of Journal</span>
+                    <span className="text-sm text-gray-300">
+                      Date - 12/01/26
+                    </span>
+                  </div>
+                  <span className="text-lg">Mood</span>
+                </div>
+                <span className="text-lg">content .....</span>
+              </div>
+            </div>
+            <div className="col-span-1">
+              <Calendar
+                mode="single"
+                // selected={date}
+                // onSelect={setDate}
+                className="rounded-lg border w-full"
+              />
+            </div>
           </div>
         </motion.main>
       </div>
